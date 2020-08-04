30d94f782cead780e023615bd84cf2780ec5ec02
Yamid Cueto <ycueto.extreme@gmail.com>
2020-08-04 12:47:03 -0500

    initial commit

---
+++ src/main/java/com/muverang/co/test/coincidences/CoincidenceFilter.java
@@ -0,0 +1,28 @@
+package com.muverang.co.test.coincidences;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/coincidences")
+public class CoincidenceFilter {
+    
+    private static final Logger log = LoggerFactory.getLogger(CoincidenceFilter.class);
+
+    @Autowired
+    private CoincidenceFilterService service;
+
+    @RequestMapping("/findwords")
+    public List<Map.Entry<String, Integer>> showWords() throws IOException {
+        return service.readWords();
+    }
+
+    
+}
---
+++ src/main/java/com/muverang/co/test/coincidences/CoincidenceFilterService.java
@@ -0,0 +1,42 @@
+package com.muverang.co.test.coincidences;
+
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class CoincidenceFilterService {
+
+    public List<Map.Entry<String, Integer>> readWords() throws IOException {
+        File file = new File("src/main/resources/static/el_quijote.txt");
+        FileInputStream fileStream = new FileInputStream(file);
+        InputStreamReader streamReader = new InputStreamReader(fileStream);
+        BufferedReader reader = new BufferedReader(streamReader);
+
+        Map<String, Integer> map = new HashMap<>();
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            String[] words = line.split("\\s+");
+            for (String w: words) {
+                String word = w.toLowerCase();
+                if (map.containsKey(word)) {
+                    map.put(word, map.get(word) + 1);
+                } else {
+                    map.put(word, 1);
+                }
+            }
+        }
+
+        List<Map.Entry<String, Integer>> top10 = new ArrayList<>(map.entrySet())
+            .stream()
+            .sorted(((e1, e2) -> e2.getValue().compareTo(e1.getValue())))
+            .limit(10)
+            .collect(Collectors.toList());
+
+        return top10;
+    }
+}
