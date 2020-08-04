5d768f7d6e6eefbd5d46f3f58506f489c5374a8a
Yamid Cueto <ycueto.extreme@gmail.com>
2020-08-04 15:18:00 -0500

    add webscoket configuration

--- src/main/java/com/muverang/co/test/config/FiltersConfig.java
+++ src/main/java/com/muverang/co/test/config/FiltersConfig.java
@@ -1,4 +1,4 @@
-package com.muverang.co.test.util;
+package com.muverang.co.test.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
