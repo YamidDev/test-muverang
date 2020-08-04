e301943a5bedb5f48f7af43e29a5971860857a91
Yamid Cueto <ycueto.extreme@gmail.com>
2020-08-04 15:02:18 -0500

    update concidences filter

--- src/main/java/com/muverang/co/test/coincidences/CoincidenceFilterService.java
+++ src/main/java/com/muverang/co/test/coincidences/CoincidenceFilterService.java
@@ -20,8 +20,7 @@ public class CoincidenceFilterService {
         String line;
 
         while ((line = reader.readLine()) != null) {
-            /*String[] words = line.split("\\s+");*/
-            String[] words = Arrays.stream(line.split("[!¡¿?.,:;()'\"]+"))
+            String[] words = Arrays.stream(line.split("[!¡¿?.,:;()´'\"]+"))
                     .collect(Collectors.joining(" ")).split("\\s+");
             for (String w: words) {
                 String word = w.toLowerCase();
